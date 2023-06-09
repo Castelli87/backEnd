fda6633adc9f0ec2911a90a4f6db07f1f590d916
Castelli87 <davidecastelli87@hotmail.com>
2023-06-09 16:53:55 +0100

    Backend connected with react native, dispalying the users from the database in the log

--- front-end/App.js
+++ front-end/App.js
@@ -1,7 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 export default function App() {
+
+  const fecthApi =()=>{
+    axios.get('http://192.168.0.11:3000/users').then((res)=>{
+      console.log(res.data)
+    }).catch((err)=>{console.log(err)})
+  }
+
+
+  useEffect(()=>{
+    fecthApi()
+
+    
+  },[])
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start workin on your app!</Text>
