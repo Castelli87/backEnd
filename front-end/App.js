0df8ec2115e0a4cdf0a15b3ff40f8926f02a26b6
Anthony Church <ag.church21@gmail.com>
2023-06-12 16:59:18 +0100

    Setup first components and displaying navigation.

--- front-end/App.js
+++ front-end/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet} from "react-native";
 import axios from "axios";
 import { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from "./components/Home";
+import Home from "./pages/Home";
+import { UserProfile } from "./pages/UserProfile";
+import { Vans } from "./pages/Vans";
 
 const Stack = createNativeStackNavigator();
 
@@ -28,6 +30,9 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="UserProfile" component={UserProfile} />
+        <Stack.Screen name="Vans" component={Vans
+        } />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- front-end/components/Nav.jsx
+++ front-end/components/Nav.jsx
@@ -1,3 +1,15 @@
-function Nav() {}
+import { ScreenContainer } from "react-native-screens";
+import { Button, View } from "react-native";
+import Home from "../pages/Home";
+
+function Nav({navigation}) {
+    return (
+        <View>
+        <Button title="Home" onPress={() => navigation.navigate("Home")} />
+        <Button title="User Profile" onPress={() => navigation.navigate("UserProfile")} />
+    </View>
+    )
+    
+}
 
 export default Nav;
