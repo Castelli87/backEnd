3b073e4e6d50c81caf932b1b1224f81970b070a4
779carney <ellemaecarney123@gmail.com>
2023-06-12 14:20:23 +0100

    made changes to pull request

--- back-end/data/users.js
+++ back-end/data/users.js
@@ -1,10 +1,10 @@
 const users = [
     {
         username: "username1",
-        passowrd: "password1",
+        password: "password1",
         address: {
             region: "barnsley",
-            postcode: "s70 4qr"
+            postcode: "s704qr"
         },
         firstName: "Ezekiel",
         lastName: "Hawkins",
@@ -14,53 +14,53 @@ const users = [
     },
     {
         username: "username2",
-        passowrd: "password2",
+        password: "password2",
         address: {
             region: "portsmouth",
-            postcode: "po16 9xb"
+            postcode: "po169xb"
         },
         firstName: "Chaim",
         lastName: "Whitfield",
-        email: "mi.lacinia@aol.ca",
+        email: "mi.lacinia@aol.com",
         phoneNumber: "07396600001",
         image: "https://static.vecteezy.com/system/resources/previews/005/544/718/original/profile-icon-design-free-vector.jpg"
     },
     {
         username: "username3",
-        passowrd: "password3",
+        password: "password3",
         address: {
             region: "birmingham",
-            postcode: "b92 9ed"
+            postcode: "b929ed"
         },
         firstName: "Ian",
         lastName: "Hooper",
-        email: "vulputate.lacus.cras@protonmail.couk",
+        email: "vulputate.lacus.cras@protonmail.co.uk",
         phoneNumber: "07396909901",
         image: "https://static.vecteezy.com/system/resources/previews/005/544/718/original/profile-icon-design-free-vector.jpg"
     },
     {
         username: "username4",
-        passowrd: "password4",
+        password: "password4",
         address: {
             region: "peterborough",
-            postcode: "pe7 3el"
+            postcode: "pe73el"
         },
         firstName: "Hoyt",
         lastName: "Ewing",
-        email: "maecenas@yahoo.edu",
+        email: "maecenas@yahoo.com",
         phoneNumber: "07396902222",
         image: "https://static.vecteezy.com/system/resources/previews/005/544/718/original/profile-icon-design-free-vector.jpg"
     },
     {
         username: "username5",
-        passowrd: "password5",
+        password: "password5",
         address: {
             "region": "liverpool",
-            "postcode": "l18 9tn"
+            "postcode": "l189tn"
         },
         firstName: "Kirestin",
         lastName: "Frank",
-        email: "lorem.ipsum@icloud.edu",
+        email: "lorem.ipsum@icloud.com",
         phoneNumber: "07396904444",
         image: "https://static.vecteezy.com/system/resources/previews/005/544/718/original/profile-icon-design-free-vector.jpg"
     }
