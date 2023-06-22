0efa970ebf6b779487dcc6341d2555e2cd3f5c67
Castelli87 <davidecastelli87@hotmail.com>
2023-06-22 13:58:38 +0100

    Style Done

--- front-end/pages/Home.jsx
+++ front-end/pages/Home.jsx
@@ -1,5 +1,12 @@
 import { useNavigation } from "@react-navigation/native";
-import { Button, Text, View, ImageBackground,StyleSheet} from "react-native";
+import {
+  Button,
+  Text,
+  View,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import { UserContext } from "../App";
 import { useContext } from "react";
 
@@ -7,17 +14,30 @@ function Home() {
   const { navigate } = useNavigation();
   const { currentUser, setCurrentUser } = useContext(UserContext);
   return (
-    <ImageBackground source={require("../assets/homePage.jpg")} style={styles.backgroundImage}>
+    <ImageBackground
+      source={require("../assets/homePage.jpg")}
+      style={styles.backgroundImage}
+    >
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         {currentUser === "" ? (
           <Text style={styles.text}>Welcome</Text>
         ) : (
           <Text style={styles.text}>Welcome {currentUser.user.firstName}</Text>
         )}
-        <Button
-          title="Advertise a Van"
+
+        <TouchableOpacity
           onPress={() => navigate("AdvertiseVan")}
-        />
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Advertise a Van</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigate("Vans")}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>  View all Vans  </Text>
+        </TouchableOpacity>
+
       </View>
     </ImageBackground>
   );
@@ -38,5 +58,19 @@ const styles = StyleSheet.create({
     color: "white",
     marginBottom: 20,
   },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "#2196F3",
+    marginVertical: 5,
+  },
+  buttonText:{
+    color: "white",
+    fontWeight:'bold'
+  }
 });
 export default Home;
